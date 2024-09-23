9da73203514a1740637ec739cc0d348f239063b1
Jonnalagadda Lakshmi Prasanna <142923910+jlakshmiprasanna@users.noreply.github.com>
2024-09-23 17:03:43 +0530

    Add files via upload

---
+++ task.js
@@ -0,0 +1,55 @@
+// (Basic arithmetic operators)
+// (addition"+")
+let a=5;
+let b=10;
+let add=a+b;
+console.log("Addition:",add);
+// (subtraction"-")
+let c=5;
+let d=10;
+let sub=c-d;
+console.log("Subtraction:",sub);
+// (Multiplication)
+let e=5;
+let f=10;
+let product=e*f;
+console.log("Multiplication:",product);
+// (Division)
+let g=5;
+let h=10;
+let quotient =g/h;
+console.log("Division:",quotient);
+// (Exponentian)
+let i=5;
+let j=10;
+let expo=i**j;
+console.log("Exponentian:",expo);
+
+
+// (NUMBERS)
+
+// (toString)
+var z=42;
+console.log(toString(z));
+
+// (parseInt)
+var y=999.99;
+console.log(parseInt(y));
+
+// (parseFloat)
+var x=999.99;
+console.log(parseFloat(x));
+
+// (isFinite)
+var w=123.567;
+console.log(isFinite(w));
+var w="Prassu";
+console.log(isFinite(w));
+
+// (toFixed)
+var v=3.1425;
+console.log(v.toFixed(2));
+
+// (toPrecision)
+var u=3.1425;
+console.log(u.toPrecision(10));
